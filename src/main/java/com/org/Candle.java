dcc75043864983fa257186784bc073829108fb83
st10434057 <st10434057@vcconnect.edu.za>
2024-08-13 12:07:29 +0200

    Matt and X making Candle Class

--- src/main/java/com/org/Candle.java
+++ src/main/java/com/org/Candle.java
@@ -1,4 +1,28 @@
 package com.org;
 
 public class Candle {
+
+    private String colour; //Matteo Declared Candle data fields
+    private double height;
+    private double price;
+
+    public String getColour() { //Xavier declared getters and setters
+        return colour;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
