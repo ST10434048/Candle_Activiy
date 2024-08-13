e7a25d78301be6c7fcf03a6cd5cd09e295350903
ST10442063 <ST10442063@vcconnect.edu.za>
2024-08-13 12:00:26 +0200

    Override price

--- src/main/java/com/org/Scented_Candle.java
+++ src/main/java/com/org/Scented_Candle.java
@@ -1,13 +1,20 @@
 package com.org;
 
 public class Scented_Candle extends Candle {
+    //ST10434048
     protected String scent;
+
     public void setScent(String scent) {
         this.scent = scent;
     }
+
     protected String getScent() {
         return scent;
     }
 
-
+//ST10442063
+    @Override
+    public void Price(int height) {
+        double Price = height * 3;
+    }
 }
