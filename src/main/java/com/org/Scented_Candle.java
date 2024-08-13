6c08b5dacc9b72e0e47f7e54801c601df4d0f9f0
Pieter-ST10434048 <ST10434048@vcconnect.edu.za>
2024-08-13 11:50:52 +0200

    Update

--- src/main/java/com/org/Scented_Candle.java
+++ src/main/java/com/org/Scented_Candle.java
@@ -1,4 +1,13 @@
 package com.org;
 
-public class Scented_Candle {
+public class Scented_Candle extends Candle {
+    protected String scent;
+    public void setScent(String scent) {
+        this.scent = scent;
+    }
+    protected String getScent() {
+        return scent;
+    }
+
+
 }
